8dd4b446761e8b776b9569fc1bc007b264725fab
mdsab <mdsab@LAPTOP-9IUMOQI7>
2021-07-13 15:12:52 -0400

    Added Stage 1 for Mario Game

---
+++ src/Mario/Stage1.java
@@ -0,0 +1,14 @@
+package Mario;
+
+public class Stage1 {
+
+	public static void main(String[] args) {
+		// Stage 1 design by Sabeel Khan
+		
+		System.out.println("Super Mario");
+		
+		System.out.println("Game Start");
+
+	}
+
+}
